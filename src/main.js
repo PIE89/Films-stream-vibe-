f9c8a95e5fa61a30647d70181703ee2c0c5d5ae0
Ivan Popov <spyder34@yandex.ru>
2026-04-15 17:54:20 +0300

    added logic to Select component

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import SliderCollection from "./modules/SliderCollection"
 import TabsCollections from "./modules/TabsCollections"
 import VideoPlayerCollection from "./modules/VideoPlayerCollection"
 import InputMaskCollection from "./modules/InputMaskCollections"
+import SelectCollection from "./modules/SelectCollections"
 
 new BurgerButtonActive()
 new SliderCollection()
 new TabsCollections()
 new VideoPlayerCollection()
 new InputMaskCollection()
+new SelectCollection()
